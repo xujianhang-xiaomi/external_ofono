e969c15c7ec43cc9c6966d332d9e1db48a8b55fe
xiaojun3 <xiaojun3@xiaomi.com>
2023-03-30 11:52:59 +0800

    ofono/phonebook: fix resource leak & dereference issues
    Change-Id: Ia3e923b610b021ed731b545aa3e626295bf6aefb

--- drivers/rilmodem/phonebook.c
+++ drivers/rilmodem/phonebook.c
@@ -252,9 +252,14 @@ static struct phonebook_entry *handle_adn(size_t len, const unsigned char *msg,
 	g_tree_insert(ref->phonebook, GINT_TO_POINTER(adn_idx), new_entry);
 
 	if (extension_record != UNUSED) {
+		const struct pb_file_info *f_info = ext1_info(ref->pb_files);
+		if (f_info == NULL)
+			return new_entry;
+
 		struct record_to_read *ext_rec =
 			g_try_malloc0(sizeof(*ext_rec));
-		const struct pb_file_info *f_info = ext1_info(ref->pb_files);
+		if (ext_rec == NULL)
+			return new_entry;
 
 		if (ext_rec && f_info) {
 			ext_rec->file_id = f_info->file_id;
@@ -330,9 +335,14 @@ static struct fdn_entry *handle_fdn(size_t len, const unsigned char *msg,
 	g_tree_insert(ref->phonebook, GINT_TO_POINTER(fdn_idx), new_entry);
 
 	if (extension_record != UNUSED) {
+		const struct pb_file_info *f_info = ext2_info(ref->pb_files);
+		if (f_info == NULL)
+			return new_entry;
+
 		struct record_to_read *ext_rec =
 			g_try_malloc0(sizeof(*ext_rec));
-		const struct pb_file_info *f_info = ext2_info(ref->pb_files);
+		if (ext_rec == NULL)
+			return new_entry;
 
 		if (ext_rec && f_info) {
 			ext_rec->file_id = f_info->file_id;
@@ -496,9 +506,14 @@ static void handle_anr(size_t len,
 	DBG("extension_record %d aas %d", extension_record, aas_record);
 
 	if (extension_record != UNUSED) {
+		const struct pb_file_info *f_info = ext1_info(ref->pb_files);
+		if (f_info == NULL)
+			return;
+
 		struct record_to_read *ext_rec =
 			g_try_malloc0(sizeof(*ext_rec));
-		const struct pb_file_info *f_info = ext1_info(ref->pb_files);
+		if (ext_rec == NULL)
+			return;
 
 		if (ext_rec && f_info) {
 			ext_rec->file_id = f_info->file_id;
@@ -564,9 +579,14 @@ static void handle_ext1(size_t len, const unsigned char *msg,
 	/* Check if there is more extension data */
 	next_extension_record = msg[12];
 	if (next_extension_record != UNUSED) {
+		const struct pb_file_info *f_info = ext1_info(ref->pb_files);
+		if (f_info == NULL)
+			return;
+
 		struct record_to_read *ext_rec =
 			g_try_malloc0(sizeof(*ext_rec));
-		const struct pb_file_info *f_info = ext1_info(ref->pb_files);
+		if (ext_rec == NULL)
+			return;
 
 		if (ext_rec && f_info) {
 			DBG("next_extension_record %d", next_extension_record);
@@ -638,9 +658,14 @@ static void handle_ext2(size_t len, const unsigned char *msg,
 	/* Check if there is more extension data */
 	next_extension_record = msg[12];
 	if (next_extension_record != UNUSED) {
+		const struct pb_file_info *f_info = ext2_info(ref->pb_files);
+		if (f_info == NULL)
+			return;
+
 		struct record_to_read *ext_rec =
 			g_try_malloc0(sizeof(*ext_rec));
-		const struct pb_file_info *f_info = ext2_info(ref->pb_files);
+		if (ext_rec == NULL)
+			return;
 
 		if (ext_rec && f_info) {
 			DBG("next_extension_record %d", next_extension_record);
@@ -1120,12 +1145,20 @@ static void start_sim_app_read(struct cb_data *cbd)
 	}
 
 	ref_rec->phonebook = g_tree_new(comp_int);
+	if (ref_rec->phonebook == NULL) {
+		ofono_error("%s: OOM", __func__);
+		g_free(ref_rec);
+		export_and_return(FALSE, cbd);
+		return;
+	}
 
 	/* Only EF_ADN and EF_EXT1 read for SIM */
 
 	f_info = g_try_malloc0(sizeof(*f_info));
 	if (f_info == NULL) {
 		ofono_error("%s: OOM", __func__);
+		g_tree_destroy(ref_rec->phonebook);
+		g_free(ref_rec);
 		export_and_return(FALSE, cbd);
 		return;
 	}
@@ -1138,6 +1171,9 @@ static void start_sim_app_read(struct cb_data *cbd)
 	f_ext1 = g_try_malloc0(sizeof(*f_ext1));
 	if (f_ext1 == NULL) {
 		ofono_error("%s: OOM", __func__);
+		g_tree_destroy(ref_rec->phonebook);
+		g_free(ref_rec);
+		g_free(f_info);
 		export_and_return(FALSE, cbd);
 		return;
 	}
@@ -1231,12 +1267,20 @@ static void ril_read_fdn_entries(struct ofono_phonebook *pb,
 	}
 
 	ref_rec->phonebook = g_tree_new(comp_int);
+	if (ref_rec->phonebook == NULL) {
+		ofono_error("%s: OOM", __func__);
+		g_free(ref_rec);
+		fdn_export_and_return(FALSE, cbd);
+		return;
+	}
 
 	/* Only EF_FDN and EF_EXT2 read for USIM */
 
 	f_fdn = g_try_malloc0(sizeof(*f_fdn));
 	if (f_fdn == NULL) {
 		ofono_error("%s: OOM", __func__);
+		g_tree_destroy(ref_rec->phonebook);
+		g_free(ref_rec);
 		fdn_export_and_return(FALSE, cbd);
 		return;
 	}
@@ -1247,6 +1291,9 @@ static void ril_read_fdn_entries(struct ofono_phonebook *pb,
 	f_ext2 = g_try_malloc0(sizeof(*f_ext2));
 	if (f_ext2 == NULL) {
 		ofono_error("%s: OOM", __func__);
+		g_tree_destroy(ref_rec->phonebook);
+		g_free(ref_rec);
+		g_free(f_fdn);
 		fdn_export_and_return(FALSE, cbd);
 		return;
 	}
@@ -1287,7 +1334,7 @@ static void ril_update_fdn_entry(struct ofono_phonebook *pb,
 				ofono_phonebook_update_cb_t cb, void *data)
 {
 	struct pb_data *pbd = ofono_phonebook_get_data(pb);
-	struct ofono_phone_number *number = NULL;
+	struct ofono_phone_number phone_number;
 	int rec_len, file_len, rec_count;
 	unsigned char effdn[255];
 	struct cb_data *cbd;
@@ -1311,9 +1358,9 @@ static void ril_update_fdn_entry(struct ofono_phonebook *pb,
 
 	cbd = cb_data_new(cb, data, pb);
 
-	string_to_phone_number(new_number, number);
+	string_to_phone_number(new_number, &phone_number);
 
-	sim_adn_build(effdn, rec_len, number, identifier);
+	sim_adn_build(effdn, rec_len, &phone_number, identifier);
 
 	ofono_sim_write(pbd->sim_context, SIM_EFFDN_FILEID,
 			fdn_update_cb, OFONO_SIM_FILE_STRUCTURE_FIXED,
